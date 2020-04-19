06c48d4bbed94da3b2b2c6e255ab06d0c5b2449f
Bruno Ribeiro <brunomestres@gmail.com>
2020-04-18 23:35:31 -0300

    convertendo templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+const conn = require('../config/DB');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  conn.query(`SELECT * FROM tb_menus ORDER BY title`, 
+    (err,results,fields)=>{
+      if(err)
+      {
+        console.log(err);
+      }
+      res.render('index', { 
+        title: 'Restaurante Saboroso!',
+        menus: results 
+      });
+  });
 });
+
+router.get('/contacts', function(req,res,next){
+  res.render('contact', { title: 'Restaurante Saboroso!' });
+});
+router.get('/menus', function(req,res,next){
+  res.render('menu', { title: 'Restaurante Saboroso!' });
+});
+router.get('/reservations', function(req,res,next){
+  res.render('reservation', { title: 'Restaurante Saboroso!' });
+});
+router.get('/services', function(req,res,next){
+  res.render('services', { title: 'Restaurante Saboroso!' });
+});
+
 
 module.exports = router;
